4fbe764ed89daa8a0fada5189a226cfcf681a585
FASTUDERHSLU\Lian Studer <lianstuder12@gmail.com>
2020-11-26 11:10:59 +0100

    seperate db connection

--- views/links.php
+++ views/links.php
@@ -1,5 +1,16 @@
 <ul class="blogs-list">
-<?php foreach ($urls as $url): ?>
+<?php
+$remoteDbUser = "d041e_listuder";
+$remoteDbPass = "12345_Db!!!";
+
+$pdo = new PDO("mysql:host=mysql2.webland.ch;dbname=d041e_listuder", $remoteDbUser, $remoteDbPass, [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+]);
+
+$sqlQuery = $pdo->query("SELECT * FROM `blog_url`");
+$urls = $sqlQuery->fetchAll();
+foreach ($urls as $url): ?>
     <li>
         <a target="_blank" href="<?= htmlspecialchars($url["blogUrl"]) ?>">
             <?= htmlspecialchars($url["blogAuthor"]) ?>
