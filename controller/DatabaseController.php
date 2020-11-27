48c8d4cb3bae82ab5af5f45a202e369154e06772
FASTUDERHSLU\Lian Studer <lianstuder12@gmail.com>
2020-11-27 11:59:08 +0100

    change db name

--- controller/DatabaseController.php
+++ controller/DatabaseController.php
@@ -2,7 +2,7 @@
 $user = 'd041e_listuder';
 $password = '12345_Db!!!';
 
-$pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=blog', $user, $password, [
+$pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=d041e_listuder', $user, $password, [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 ]);
