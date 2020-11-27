6567e18c7960a2138f30ecc34c22a2dfd0eb8f6a
FASTUDERHSLU\Lian Studer <lianstuder12@gmail.com>
2020-11-27 11:57:59 +0100

    change credentials

--- controller/DatabaseController.php
+++ controller/DatabaseController.php
@@ -1,6 +1,6 @@
 <?php 
-$user = 'root';
-$password = '';
+$user = 'd041e_listuder';
+$password = '12345_Db!!!';
 
 $pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=blog', $user, $password, [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
